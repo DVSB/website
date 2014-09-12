a9fabd19cdbf0102f2a20a7ca2a3ccc1e8ff6f77
TGates <tgates@mach-hosting.com>
2014-09-11 23:55:34 -0400

    reverted back to original

--- app/views/partials/team.blade.php
+++ app/views/partials/team.blade.php
@@ -5,7 +5,7 @@
     <div class="col-md-3 margin-btm20">
         <img src="{{ Gravatar::make($member['gravatar_email'])->defaultGravatar('mm')->size(220)->get() }}" class="img-responsive" alt="">
         <div class="person-desc">
-            <h4>{{ $member['name'] }} - {{ $member['forum_name'] }}</h4>
+            <h4>{{ $member['name'] }}</h4>
              <p>{{ $member['role'] }}</p>
             <ul class="list-unstyled list-inline social">
                 @if(isset($member['social_facebook']) && $member['social_facebook'] != '')
