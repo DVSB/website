cdf19f5da3a630df2811d494664546c6280558be
Pascal Peyremorte <5050@users.noreply.github.com>
2014-09-13 11:20:48 +0200

    Moving to Sentora group paypal acccount

--- app/views/partials/paypal.blade.php
+++ app/views/partials/paypal.blade.php
@@ -1,13 +1,7 @@
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-    <input type="hidden" name="cmd" value="_donations">
-    <input type="hidden" name="business" value="bobbyallen.uk@gmail.com">
-    <input type="hidden" name="lc" value="GB">
-    <input type="hidden" name="item_name" value="Sentora Control Panel Project">
-    <input type="hidden" name="item_number" value="website">
-    <input type="hidden" name="no_note" value="0">
-    <input type="hidden" name="currency_code" value="USD">
-    <input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest">
-    <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
-    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+  <input type="hidden" name="cmd" value="_s-xclick">
+  <input type="hidden" name="hosted_button_id" value="WZ2PEH84756SJ">
+  <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal.">
+  <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
 </form>
 <p>&nbsp;</p>
