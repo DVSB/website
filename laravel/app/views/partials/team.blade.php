48cbf57449910d7ea56deae74dd7b452d10ea7bc
5050 <p.peyremorte@free.fr>
2015-02-18 08:27:57 +0100

    Add forum name & profile
    - updated from server, and also simplifyed test like if (isset(x) and x
    !="")  to  if (!empty(x))

--- laravel/app/views/partials/team.blade.php
+++ laravel/app/views/partials/team.blade.php
@@ -6,21 +6,24 @@
         <img src="{{ Gravatar::make($member['gravatar_email'])->defaultGravatar('mm')->size(220)->get() }}" class="img-responsive" alt="">
         <div class="person-desc">
             <h4>{{ $member['name'] }}</h4>
+             @if(!empty($member['forum_name']))
+             <p>Forum profile: <a href="{{ $member['forum_profile'] }}" target="_blank">{{ $member['forum_name'] }}</a></p>
+             @endif
              <p>{{ $member['role'] }}</p>
             <ul class="list-unstyled list-inline social">
-                @if(isset($member['social_facebook']) && $member['social_facebook'] != '')
+                @if(!empty($member['social_facebook']))
                 <li><a href="{{ $member['social_facebook'] }}" target="_blank"><i class="ion-social-facebook-outline" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook profile"></i></a></li>
                 @endif
-                @if(isset($member['social_twitter']) && $member['social_twitter'] != '')
+                @if(!empty($member['social_twitter']))
                 <li><a href="{{ $member['social_twitter'] }}" target="_blank"><i class="ion-social-twitter-outline" data-toggle="tooltip" data-placement="top" title="" data-original-title="Follow on Twitter"></i></a></li>
                 @endif
-                @if(isset($member['social_googleplus']) && $member['social_googleplus'] != '')
+                @if(!empty($member['social_googleplus']))
                 <li><a href="{{ $member['social_googleplus'] }}" target="_blank"><i class="ion-social-googleplus-outline" data-toggle="tooltip" data-placement="top" title="" data-original-title="Follow on Google+"></i></a></li>
                 @endif
-                @if(isset($member['social_github']) && $member['social_github'] != '')
+                @if(!empty($member['social_github']))
                 <li><a href="{{ $member['social_github'] }}" target="_blank"><i class="ion-social-github-outline" data-toggle="tooltip" data-placement="top" title="" data-original-title="Follow on GitHub"></i></a></li>
                 @endif
-                @if(isset($member['social_siteblog']) && $member['social_siteblog'] != '')
+                @if(!empty($member['social_siteblog']))
                 <li><a href="{{ $member['social_siteblog'] }}" target="_blank"><i class="ion-social-wordpress-outline" data-toggle="tooltip" data-placement="top" title="" data-original-title="Personal website"></i></a></li>
                 @endif
             </ul>
